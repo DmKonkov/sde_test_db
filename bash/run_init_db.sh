f45690d4c4bec0499bedf9300af658baada14cc4
Dmitriy Konkov <vboxuser@Linux.myguest.virtualbox.org>
2023-06-18 18:28:11 +0300

    start pssql and load BD demo

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+docker run --name possql -d -p 5432:5432 -v ~/sde/sde_test_db/sql/init_db:/var/backups -e POSTGRES_PASSWORD=@sde_password012 -e POSTGRES_USER=test_sde -e POSTGRES_DB=demo postgres
+echo "possql started! Wait ..."
+sleep 5 
+docker exec -it possql psql -U test_sde -d demo -f /var/backups/demo.sql
+echo "DB load Finish!"
